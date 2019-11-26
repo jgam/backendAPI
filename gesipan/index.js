bc2a59aebd4b5d3f986a9171fe4c2f9fb5aaed5a
jgam <jgam@alumni.nd.edu>
2019-11-27 00:40:34 +0900

    done

---
+++ gesipan/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+
+//DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connect(
+  'mongodb+srv://jgam:19921019@cluster0-itx5s.mongodb.net/test?retryWrites=true&w=majority'
+);
+var db = mongoose.connection;
+db.once("open", functino(){
+    console.log("DB connected girit!");
+});
+db.on("error", functino(err){
+    console.log("DB ERROR : ", err);
+});
+
+//Other settings
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
+//routes
+app.use("/", require("./routes/home"));
+
+//port setting
+app.listen(3000, function(){
+    console.log("server on! localhost:"+port);
+});
