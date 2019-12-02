517cf09e53fd52afb3c2c106acbcdbce9938d105
jgam <jgam@alumni.nd.edu>
2019-12-02 18:55:30 +0900

    fixing

--- gesipan/index.js
+++ gesipan/index.js
@@ -12,24 +12,25 @@ mongoose.connect(
   'mongodb+srv://jgam:19921019@cluster0-itx5s.mongodb.net/test?retryWrites=true&w=majority'
 );
 var db = mongoose.connection;
-db.once("open", functino(){
-    console.log("DB connected girit!");
+db.once('open', function() {
+  console.log('DB connected girit!');
 });
-db.on("error", functino(err){
-    console.log("DB ERROR : ", err);
+db.on('error', function(err) {
+  console.log('DB ERROR : ', err);
 });
 
 //Other settings
-app.set("view engine", "ejs");
-app.use(express.static(__dirname+"/public"));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 //routes
-app.use("/", require("./routes/home"));
+app.use('/', require('./routes/home'));
 
 //port setting
-app.listen(3000, function(){
-    console.log("server on! localhost:"+port);
+var port = 3000;
+app.listen(port, function() {
+  console.log('server on! localhost:' + port);
 });
