eca389e1591412cb058a6f7c633b683c26e1cbf7
jgam <jgam@alumni.nd.edu>
2019-12-12 20:45:35 +0900

    [MEAN] bug fixed

--- gesipan/models/User.js
+++ gesipan/models/User.js
@@ -9,7 +9,7 @@ var userSchema = mongoose.Schema(
     username: {
       type: String,
       required: [true, 'Username is required!'],
-      match:[/^.{4,12}$/."Should be 4-12 characters!"],
+      match: [/^.{4,12}$/, 'Should be 4-12 characters!'],
       unique: true
     },
     password: {
@@ -17,16 +17,19 @@ var userSchema = mongoose.Schema(
       required: [true, 'Password is required!'],
       select: false
     },
-    name: { 
-        type: String, 
-        required: [true, 'Name is required!'],
-        match:[/^.{4,12}$/,"name should be 4-12 characters!"],
-        trim: true
+    name: {
+      type: String,
+      required: [true, 'Name is required!'],
+      match: [/^.{4,12}$/, 'name should be 4-12 characters!'],
+      trim: true
     },
-    email: { 
-        type: String,
-        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/,"Should be a valid email address!"],
-        trim: true
+    email: {
+      type: String,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/,
+        'Should be a valid email address!'
+      ],
+      trim: true
     }
   },
   {
@@ -73,37 +76,50 @@ userSchema
 
 // password validation // 3
 var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
-var passwordRegexErrorMessage = "Should be minimum 8 characters of alphabet and number combination!";
+var passwordRegexErrorMessage =
+  'Should be minimum 8 characters of alphabet and number combination!';
 userSchema.path('password').validate(function(v) {
-    var user = this;
+  var user = this;
 
-    // create user
-    if(user.isNew){
-      if(!user.passwordConfirmation){
-        user.invalidate("passwordConfirmation", "Password Confirmation is required!");
-      }
-      if(!passwordRegex.test(user.password)){
-        user.invalidate("password", passwordRegexErrorMessage);
-      } else if(user.password !== user.passwordConfirmation) {
-        user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
-      }
+  // create user
+  if (user.isNew) {
+    if (!user.passwordConfirmation) {
+      user.invalidate(
+        'passwordConfirmation',
+        'Password Confirmation is required!'
+      );
     }
-  
-    // update user
-    if(!user.isNew){
-      if(!user.currentPassword){
-        user.invalidate("currentPassword", "Current Password is required!");
-      }
-      if(user.currentPassword && !bcrypt.compareSync(user.currentPassword, user.originalPassword)){
-        user.invalidate("currentPassword", "Current Password is invalid!");
-      }
-      if(user.newPassword && !passwordRegex.test(user.newPassword)){
-        user.invalidate("newPassword", passwordRegexErrorMessage);
-      } else if(user.newPassword !== user.passwordConfirmation) {
-        user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
-      }
+    if (!passwordRegex.test(user.password)) {
+      user.invalidate('password', passwordRegexErrorMessage);
+    } else if (user.password !== user.passwordConfirmation) {
+      user.invalidate(
+        'passwordConfirmation',
+        'Password Confirmation does not matched!'
+      );
     }
-  });
+  }
+
+  // update user
+  if (!user.isNew) {
+    if (!user.currentPassword) {
+      user.invalidate('currentPassword', 'Current Password is required!');
+    }
+    if (
+      user.currentPassword &&
+      !bcrypt.compareSync(user.currentPassword, user.originalPassword)
+    ) {
+      user.invalidate('currentPassword', 'Current Password is invalid!');
+    }
+    if (user.newPassword && !passwordRegex.test(user.newPassword)) {
+      user.invalidate('newPassword', passwordRegexErrorMessage);
+    } else if (user.newPassword !== user.passwordConfirmation) {
+      user.invalidate(
+        'passwordConfirmation',
+        'Password Confirmation does not matched!'
+      );
+    }
+  }
+});
 
 //hash password //3
 userSchema.pre('save', function(next) {
