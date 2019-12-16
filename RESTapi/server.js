4ec739959526ddee80af047c55432d545743a77c
jgam <jgam@alumni.nd.edu>
2019-12-16 12:46:06 +0900

    [MEAN] API done, now look through mongoShell

--- RESTapi/server.js
+++ RESTapi/server.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var mongoose = require('mongooes');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
 
 //Database
 mongoose.Promise = global.Promise;
+console.log(process.env.MONGO_DB);
 mongoose.connect(process.env.MONGO_DB, {userMongoClient:true});
 var db = mongoose.connection;
+
+//process.env.MONGO_DB;
 
 db.once('open', function(){
     console.log('DB connected!');
@@ -22,7 +27,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res,next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'Get, POST, PUT, DELETE');
-    res.headaer('Access-Control-Allow-Headers', 'content-type');
+    res.header('Access-Control-Allow-Headers', 'content-type');
     next();
 })
 
@@ -30,7 +35,7 @@ app.use(function (req, res,next){
 app.use('/api/heroes', require('./api/heroes'));
 
 //server
-var post = 3001;
+var port = 3001;
 app.listen(port, function(){
     console.log('listening on port:' + port);
 })
