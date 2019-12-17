ea190791bdf07b54c2a9061f8a55e32e894a2461
jgam <jgam@alumni.nd.edu>
2019-12-17 17:19:42 +0900

    [post Error] in progress

--- gesipan/routes/posts.js
+++ gesipan/routes/posts.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var Post = require('../models/Post');
+var util = require("../util");
 
 //Index
 router.get('/', function(req, res) {
@@ -19,8 +20,9 @@ router.get('/', function(req, res) {
 //New
 router.get('/new', function(req, res) {
   console.log('2');
-
-  res.render('posts/new');
+  var post = req.flash("post")[0] || {};
+  var errors = req.flash("errors")[0] || {};
+  res.render("posts/new", {post:post, errors:errors});
 });
 
 //create
@@ -30,7 +32,11 @@ router.post('/', function(req, res) {
 
   Post.create(req.body, function(err, post) {
     //creates data in db(data, callback Function)
-    if (err) return res.json(err); //here post is the created data if not, then error
+    if (err) {
+      req.flash("post", req.body);
+      req.flash("errors", util.parseError(err));
+      return res.redirect("posts/new");
+    } //here post is the created data if not, then error
     res.redirect('/posts'); //automatically renders index in posts directory
   });
 });
@@ -47,20 +53,31 @@ router.get('/:id', function(req, res) {
 
 // edit
 router.get('/:id/edit', function(req, res) {
-  Post.findOne({ _id: req.params.id }, function(err, post) {
-    if (err) return res.json(err);
-    res.render('posts/edit', { post: post });
-  });
+  var post = req.flash("post")[0];
+  var errors = req.flash("errors")[0] || {};
+  if(!post){
+    Post.findOne({_id: req.params.id}, function(err, post){
+      if(err) return res.json(err);
+      res.render("posts/edit", {post:post, errors:errors});
+    });
+  }
+  else{
+    post._id = req.params.id;
+    res.render("posts/edit", { post:post, errors:errors});
+  }
 });
 
 // update
 router.put('/:id', function(req, res) {
   console.log('edit did');
   req.body.updatedAt = Date.now(); // 2
-  Post.findOneAndUpdate({ _id: req.params.id }, req.body, function(err, post) {
-    if (err) return res.json(err);
-    res.redirect('/posts/' + req.params.id);
-  });
+  Post.findOneAndUpdate({_id:req.params.id}, req.body, {runValidators:true}, function(err, post){
+    if(err){
+     req.flash("post", req.body);
+     req.flash("errors", util.parseError(err));
+     return res.redirect("/posts/"+req.params.id+"/edit");
+    }
+    res.redirect("/posts/"+req.params.id);
 });
 
 // destroy
