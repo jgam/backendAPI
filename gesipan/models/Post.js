361617d5183fc352b8775d47d149f2df747134bf
jgam <jgam@alumni.nd.edu>
2019-12-17 17:02:09 +0900

    [gesipan] post Error init

--- gesipan/models/Post.js
+++ gesipan/models/Post.js
@@ -2,13 +2,14 @@
 //this is model creation with mongo db
 
 var mongoose = require('mongoose');
+var util = require("../util");//1
 
 //schema
 var postSchema = mongoose.Schema(
   {
     //postSchema is composed of title, body, createdAt, updatedAt
-    title: { type: String, required: true },
-    body: { type: String },
+    title: { type: String, required: [true,"Title is required!"]},//2
+    body: { type: String, required:[true, "Body is required!"] },//2
     createdAt: { type: Date, default: Date.now }, //can set default
     updatedAt: { type: Date }
   },
@@ -30,19 +31,19 @@ person.save(function(err, res) {
 postSchema.virtual('createdDate').get(function() {
   //inside of postSchema, virtually access entity
   //using virtual method,
-  return getDate(this.createdAt); //update createdDate : getDate(this.createdAt)
+  return util.getDate(this.createdAt); //update createdDate : getDate(this.createdAt)
 });
 
 postSchema.virtual('createdTime').get(function() {
-  return getTime(this.createdAt);
+  return util.getTime(this.createdAt);
 });
 
 postSchema.virtual('updatedDate').get(function() {
-  return getDate(this.updatedAt);
+  return util.getDate(this.updatedAt);
 });
 
 postSchema.virtual('updatedTime').get(function() {
-  return getTime(this.updatedAt);
+  return util.getTime(this.updatedAt);
 });
 
 // model & export
--- gesipan/routes/users.js
+++ gesipan/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
+var util = require("../util"); //1
 
 // Index
 router.get('/', function(req, res) {
@@ -24,7 +25,7 @@ router.post('/', function(req, res) {
   User.create(req.body, function(err, user) {
     if (err) {
       req.flash('user', req.body);
-      req.flash('errors', parseError(err));
+      req.flash('errors', util.parseError(err));
       return res.redirect('/users/new');
     }
     res.redirect('/users');
@@ -81,10 +82,10 @@ router.put('/:username', function(req, res, next) {
       user.save(function(err, user) {
         if (err) {
           req.flash('user', req.body);
-          req.flash('errors', parseError(err));
+          req.flash('errors', util.parseError(err));//1
           return res.redirect('/users/' + req.params.username + '/edit');
         }
-        res.redirect('/users/' + user.username);
+        res.redirect('/users/' + req.params.username);
       });
     });
 });
