e7f639e8331383c8cbf9abc16000907da1492e5e
jgam <jgam@alumni.nd.edu>
2019-12-17 16:10:09 +0900

    [gesipan] login done

--- gesipan/index.js
+++ gesipan/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var flash = require('connect-falsh');
+var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require("./config/passport");//1
 var app = express();
@@ -28,6 +28,14 @@ db.on('error', function(err) {
 // Other settings
 app.set('view engine', 'ejs');
 
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(flash()); // 2 flash is an array that stores number and string in array as index and element
+app.use(session({ secret: 'MySecret', resave: true, saveUninitialized: true })); // 3
 // Passport // 2
 app.use(passport.initialize());
 app.use(passport.session()); 
@@ -40,12 +48,6 @@ app.use(function(req,res,next){
 })
 
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(flash()); // 2 flash is an array that stores number and string in array as index and element
-app.use(session({ secret: 'MySecret', resave: true, saveUninitialized: true })); // 3
 //session is to distinguish users and secret is the password for specific session.
 //routes
 app.use('/', require('./routes/home'));
